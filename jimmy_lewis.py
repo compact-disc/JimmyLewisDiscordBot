5783b8448b6d624ea32fa5a94b1dfadf28b638bb
Christopher DeRoche <46451756+compact-disc@users.noreply.github.com>
2021-05-18 17:34:07 -0400

    added drake react chance

--- jimmy_lewis.py
+++ jimmy_lewis.py
@@ -8,11 +8,14 @@ from discord_slash import SlashCommand, SlashContext
 # Import config.py with the bot_token string
 from config import bot_token
 
+# Random generator values
+import random
+
 # Create the bot object and set the command prefix for any non-slash commands
-bot = commands.Bot(command_prefix=".")
+bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 
 # Create the Slash Commands object and add the bot
-slash = SlashCommand(bot, sync_commands=True)
+slash = SlashCommand(bot)
 
 
 # On ready event to run after the bot has started
@@ -37,15 +40,15 @@ async def on_message(ctx):
     Drake ID - 200391739113078785
     :drake: ID - <:drake:821463946284367902>
     """
-    if ctx.author.id == 200391739113078785:
+    if ctx.author.id == 200391739113078785 and random.randint(0, 100) <= 1:
         drake_react = "<:drake:821463946284367902>"
         await ctx.add_reaction(drake_react)
 
 
 # Slash Command to ping the bot
 @slash.slash(name="ping", description="Check that the bot is connected and running.")
-async def ping(ctx):
-    await ctx.send(f"Pong! ({bot.latency*1000}ms)")
+async def ping(ctx: SlashContext):
+    await ctx.send(content=f"Pong! ({bot.latency*1000}ms)")
 
 
 # Start the bot
